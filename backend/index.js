0842c84591fda7f40c8d4bff3290c3837c92900a
parthivkantrodiya <parthivkatrodiya26+github@gmail.com>
2026-07-31 14:53:37 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
---
+++ frontend/src/store/slice/hostSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const hostslice  = createSlice({
+  name: "hosts",
+  initialState,
+  reducers: {},
+});
+
+export const {
+
+} = hostslice.actions;
+
+export default hostslice.reducer;
